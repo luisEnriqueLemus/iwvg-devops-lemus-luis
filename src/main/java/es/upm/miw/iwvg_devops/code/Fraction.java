dd0511827698f14cde240003e49d10a86e30e46e
Luis Lemus <luis.lemus@alumnos.upm.es>
2022-10-18 08:16:08 +0200

    added Usser and UserTest #7

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -0,0 +1,9 @@
+package es.upm.miw.iwvg_devops.code;
+
+public class Fraction {
+    public Fraction(Integer numerator, Integer denominator){};
+
+    public int decimal(){
+        return 0;
+    }
+}
